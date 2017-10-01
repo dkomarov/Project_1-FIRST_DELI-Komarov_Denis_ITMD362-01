ea596288056f446e920303abd3e472a39dee132e
dkomarovo <dkomarov@hawk.iit.edu>
2017-10-01 14:58:59 -0500

    Edit JavaScript file with uniform 2-space tab cascade

--- form.js
+++ form.js
@@ -1,34 +1,28 @@
 function confirmation() {
 
-
-		if ((document.getElementById('dob_field').value == "") && (document.getElementById('email_field').value == ""))
-		{
-			alert("The 'date of birth' and 'email address' fields are mandatory. Please make sure you have filled out the 'date of birth' and 'email address' fields.");
-			return false;
-			
-
-		}
-		else 
-		{
-			alert("Form submitted successfully! The fields will now reset.");
-			document.write("Form submitted successfully! Fields not completed were optional. Submission confirmed.");
-			
-		}
-
-
-		if ((document.getElementById('fName_field').value == "") && (document.getElementById('lName_field').value == "") && (document.getElementById('phoneNum_field').value == ""))
-  	{
-  	
-  			alert("There were empty fields! However, some fields are optional (both first & last name and 'phone number'). If you do not wish to provide optional information, click 'Submit' once more.");
-
-
-		}
-		else {
-  		alert("Form submitted successfully! The fields will now reset.");
-	  	document.write("Form submitted successfully! Fields not completed were optional. Submission confirmed.");
-		}
-
+	if ((document.getElementById('dob_field').value == "") && (document.getElementById('email_field').value == ""))
+	{
+		alert("The 'date of birth' and 'email address' fields are mandatory. Please make sure you have filled out the 'date of birth' and 'email address' fields.");
+		return false;
 	}
+	else 
+	{
+		alert("Form submitted successfully! The fields will now reset.");
+		document.write("Form submitted successfully! Fields not completed were optional. Submission confirmed.");
+			
+	}
+
+	if ((document.getElementById('fName_field').value == "") && (document.getElementById('lName_field').value == "") && (document.getElementById('phoneNum_field').value == ""))
+  {
+  	alert("There were empty fields! However, some fields are optional (both first & last name and 'phone number'). If you do not wish to provide optional information, click 'Submit' once more.");
+	}
+	else 
+	{
+  alert("Form submitted successfully! The fields will now reset.");
+	document.write("Form submitted successfully! Fields not completed were optional. Submission confirmed.");
+	}
+
+}
 
 
 
